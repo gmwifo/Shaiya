4a7489d21a7e8eb3a22705e6abc2fe33a357a43f
Wifo <102623566+cwifo@users.noreply.github.com>
2023-09-16 09:06:40 +0800

    Add files via upload

---
+++ OpenSource/Game.exe/EP5/VetManager.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+
+int Points[]
+{1,3,5,7,12,18,20,22,24,26,26,26,27,27,27,28,
+28,28,28,28,28,29,29,29,29,29,30,30,30,30,30,};
+
+unsigned u0x519DCC = 0x519DCC;
+
+__declspec(naked) void naked_0x519DC7()
+{
+	__asm
+	{
+		push edi
+		movzx edi, byte ptr ds : [0x71C1D8]
+		mov eax, [Points + edi * 4]
+		pop edi
+		mov dword ptr ds : [0x719FB8] , eax
+		jmp u0x519DCC
+	}
+}
+
+void hook::vet_manager()
+{
+	util::detour((void*)0x519DC7, naked_0x519DC7, 5);
+}
