c3db7b920d726e981afa01ecaac7140c2e10c673
Wifo <102623566+cwifo@users.noreply.github.com>
2024-01-10 13:04:26 +0800

    Update RecreationViewer.cpp

--- OpenSource/Game.exe/EP5/RecreationViewer.cpp
+++ OpenSource/Game.exe/EP5/RecreationViewer.cpp
@@ -1,4 +1,10 @@
-#include "pch.h"
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <array>
+
+#include <include/main.h>
+#include <include/util.h>
 
 unsigned u0x4AD709 = 0x4AD709;
 unsigned u0x4AD694 = 0x4AD694;
@@ -8,10 +14,10 @@ __declspec(naked) void naked_0x4AD68F()
 	{
 		add eax, 0x88
 		cmp edx, eax
-		jg viewer
+		jg _u0x4AD694
 		jmp u0x4AD694
-		viewer :
-		jmp u0x4AD694
+		_u0x4AD694 :
+		jmp u0x4AD709
 	}
 }
 
