ac39a82d94c297a483716417ab33cd86c9c98035
Wifo <102623566+cwifo@users.noreply.github.com>
2024-01-10 13:08:00 +0800

    Update VetManager.cpp

--- OpenSource/Game.exe/EP5/VetManager.cpp
+++ OpenSource/Game.exe/EP5/VetManager.cpp
@@ -1,8 +1,47 @@
-#include "pch.h"
+#define WIN32_LEAN_AND_MEAN
 
-int Points[32]
-{0,1,3,5,7,12,18,20,22,24,26,26,26,27,27,27,28,
-28,28,28,28,28,29,29,29,29,29,30,30,30,30,30};
+#include <windows.h>
+#include <array>
+
+#include <include/main.h>
+#include <include/util.h>
+
+std::array<int, 32> points =
+
+{
+  1,
+  3,
+  5,
+  7,
+  12,
+  18,
+  20,
+  22,
+  24,
+  26,
+  26,
+  26,
+  27,
+  27,
+  27,
+  28,
+  28,
+  28,
+  28,
+  28,
+  28,
+  29,
+  29,
+  29,
+  29,
+  29,
+  30,
+  30,
+  30,
+  30,
+  30,
+  30
+};
 
 unsigned u0x519DCC = 0x519DCC;
 
